37707a3bde62a8d04ed53a6543ebfad5f8cc7c15
Irina <irbris@mail.ru>
2020-08-30 20:48:35 +0300

    Add files via upload

---
+++ precache-manifest.30a3e9bc2bb76eb9d24b23a9f81f5417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd41c1232a039c645b77fc2fd66a96c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf7dfa828f207d6ac15",
+    "url": "/static/css/main.92906702.chunk.css"
+  },
+  {
+    "revision": "d3d136bfbb29e83eb6a7",
+    "url": "/static/js/2.3af6a455.chunk.js"
+  },
+  {
+    "revision": "552fc37e66e521b8ef1b4259470f149f",
+    "url": "/static/js/2.3af6a455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf7dfa828f207d6ac15",
+    "url": "/static/js/main.f2e12998.chunk.js"
+  },
+  {
+    "revision": "4c3f3886744812312d98",
+    "url": "/static/js/runtime-main.a9097ddf.js"
+  }
+]);
